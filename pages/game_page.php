917453d5c34b3daaae2c8d9a836112d17f8a2d05
Sage Pellinen <sagepellinen@gmail.com>
2019-04-08 17:35:13 -0500

    forms for edit_account

--- pages/game_page.php
+++ pages/game_page.php
@@ -7,7 +7,19 @@
 </head>
 <body>
 <?php
-
+require_once("../classes/Character.php");
+$character1 = "";
+$character2 = "";
+$character3= "";
+$character4 = "";
+$game_master = "";
+$game_id = "";
+echo $character1;
+echo $character2;
+echo $character3;
+echo $character4;
+echo $game_master;
+echo $game_id;
 ?>
 </body>
 </html>

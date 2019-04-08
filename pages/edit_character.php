65965b3a988fbd00888c5d1ec27d4da856c45413
Tate Mioton <tatemioton@outlook.com>
2019-04-07 20:36:53 -0500

    Start to Add CSS to Character Edit Page
    Signed-off-by: Tate Mioton <tatemioton@outlook.com>

--- pages/edit_character.php
+++ pages/edit_character.php
@@ -7,8 +7,238 @@
     // Get user info; Create Character and Player classes here.
     ?>
     <title><?php echo "$characterName ($username)"; ?></title>
+    <link rel="stylesheet" href="../styles/general.css">
+    <style>
+        body {
+            background: black;
+        }
+
+        .content {
+            width: 1024px;
+        }
+
+        .col.label {
+            width: 256px;
+        }
+
+        label.h2 {
+            font-size: 1.5em;
+        }
+
+        input.h2 {
+            font-size: 1.5em;
+        }
+
+        label.h3 {
+            font-size: 1.3em;
+        }
+
+        input.h3 {
+            font-size: 1.3em;
+        }
+
+        label, a {
+            font-size: 13px;
+        }
+
+        input {
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 4px;
+            border-radius: 4px;
+            color: #111111;
+            width: calc(100% - 18px);
+            /*width: 100%;*/
+            border: 2px solid #333333;
+            padding: 4px 8px;
+            font-size: 13px;
+        }
+
+        textarea {
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 4px;
+            border-radius: 4px;
+            color: #111111;
+            width: calc(100% - 18px);
+            border: 2px solid #333333;
+            padding: 4px 8px;
+            font-size: 13px;
+            resize: none
+        }
+
+        input.name {
+            width: 256px;
+        }
+
+        input.number {
+            width: 32px;
+        }
+
+        input.inc-button {
+            background-color: orange;
+            text-align: center;
+        }
+
+        h3, h4, p {
+            margin-top: 0;
+            margin-bottom: 0;
+        }
+
+        p {
+            font-size: 16px;
+        }
+
+        hr {
+            margin-top: 2px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
+<?php
+require_once "../classes/Character.php";
+if (isset($_SESSION['character'])) {
+    $character = unserialize($_SESSION['character']);
+}
+else {
+    $character = new Character(true);
+    $character->name = "Sebille";
+}
+?>
+<div class="content">
+    <form method="post" action="edit_character.php" name="edit">
+    <div class="wide-4">
+        <!--Names-->
+        <div class="wide-2">
+            <h1><?php echo $username ?></h1>
+
+            <input
+                    class="name h2" id="name" type="text"
+                    value="<?php echo $character->name?>"
+                    maxlength="32"
+            >
+            <br>
+            <input
+                    class="name h3" id="level" type="number"
+                    max="50" value="<?php echo $character->level ?>"
+            >
+        </div>
+
+        <!--Background-->
+        <div class="wide-2">
+            <h3>Background</h3>
+            <hr>
+            <textarea form="edit" rows="4"><?php echo $character->background ?></textarea>
+        </div>
+
+        <!--Abilities, Attributes, and Talents-->
+        <div class="col-container">
+            <!--Abilities-->
+            <div class="col w-50">
+                <h3>Combat Abilities</h3>
+                <hr>
+                <?php
+                $combatAbilities = array(
+                    "Weapons" => $weaponCombatAbilityNames,
+                    "Defence" => $defenceCombatAbilityNames,
+                    "Skills"  => $skillCombatAbilityNames
+                );
+                foreach ($combatAbilities as $key => $section) {
+                    echo "<h4>$key</h4>";
+                    echo "<div class='section'>";
+                    foreach ($section as $skill) {
+                        $value = $character->getAbility($skill);
+                        echo "<div class='col-container'>";
+
+                        $desc = addslashes($abilityDescriptions[$skill]);
+                        echo    "<div class=\"col w-75\" id=\"$skill\" onclick=\"showAbilityDescription('$skill', '$desc')\">";
+                        echo        "<p>$skill</p>";
+                        echo    "</div>";
+
+                        echo    "<div class=\"col w-25\">";
+                        echo        "<input type='text' class='number' value='$value'>";
+                        echo    "</div>";
+                        echo "</div>";
+                    }
+                    echo "</div>";
+                }
+                ?>
+
+                <h3>Civil Abilities</h3>
+                <hr>
+                <?php
+                $civilAbilities = array(
+                    "Personality"=>['Bartering', 'Lucky Charm', 'Persuasion'],
+                    "Craftsmanship"=>['Loremaster', 'Telekinesis'],
+                    "Nasty Deeds"=>['Sneaking', 'Thievery']
+                );
+                foreach ($civilAbilities as $key => $section) {
+                    echo "<h4>$key</h4>";
+                    echo '<div class="section">';
+                    foreach ($section as $skill) {
+                        $value = $character->getAbility($skill);
+                        echo '<div class="col-container">';
+
+                        $desc = addslashes($abilityDescriptions[$skill]);
+                        $div_id = $skill . "_inc";
+                        echo    "<div class='col w-75' id='$div_id' onclick=\"showAbilityDescription('$div_id', '$skill', '$desc')\">";
+                        echo        "<p>$skill</p>";
+                        echo    "</div>";
+
+                        echo    "<div class=\"col w-25\">";
+                        echo        "<div class='col-container'>";
+                        echo            "<input id='$skill' type='text' class='col number' value='$value' readonly='true'>";
+                        echo            "<input class='col inc-button' type='button' value='+' onclick=\"add('$skill')\">";
+                        echo            "<input class='col inc-button' type='button' value='-' onclick=\"minus('$skill')\">";
+                        echo        "</div>";
+                        echo    "</div>";
+                        echo "</div>";
+                    }
+                    echo '</div>';
+                }
+                ?>
+            </div>
+
+            <!--Attributes and Talents-->
+            <div class="col w-50">
+
+            </div>
+        </div>
+    </div>
+    </form>
+</div>
+<script>
+    let id_array = [];
+
+    function add(id) {
+        let value = id_array[id];
+        if (value === undefined) {
+            value = 1;
+        }
+        else {
+            value++;
+        }
+        document.getElementById(id).value = value;
+        id_array[id] = value;
+    }
+
+    function minus(id){
+        let value = id_array[id];
+        if (value === undefined) {
+            value = 0;
+        }
+        else {
+            if (value > 0) {
+                value--;
+            }
+            else {
+                value = 0;
+            }
+        }
+        document.getElementById(id).value = value;
+        id_array[id] = value;
+    }
+</script>
+<?php exit(); ?>
 <div>
     **Attributes**
 

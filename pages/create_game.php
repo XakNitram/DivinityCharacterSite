88ea779b0446502fe657d22e614aab22af71827e
Tate Mioton <tatemioton@outlook.com>
2019-04-09 21:38:21 -0500

    Add CSS to Game Creation Page
    Signed-off-by: Tate Mioton <tatemioton@outlook.com>

--- pages/create_game.php
+++ pages/create_game.php
@@ -1,157 +1,85 @@
 <!DOCTYPE html>
 <html lang="en-us">
 <head>
-    <link rel="stylesheet" type="text/css" href="../styles/general.css">
     <title>Create Game</title>
-    <style>
-        select, option, input, textarea {
-            color: black;
-        }
-        <?php
-require_once "../classes/Game.php";
+    <link rel="stylesheet" type="text/css" href="../styles/general.css">
+    <link rel="stylesheet" type="text/css" href="../styles/newgame.css">
+    <?php
+    require_once "../classes/Game.php";
 
 
     $gm_username = "";
-    $gm_email = "";
     $gm_password = "";
     $player_num = "";
     $game_description = "";
-    $email_error = "";
-    $passError = "";
-    $showError = false;
-    $usernameError= "";
-    $description_error = "";
-    if (isset($_POST["submit"])) {
-        $gm_username = $_POST["gm_username"];
-        $gm_email = $_POST["gm_email"];
-        $player_num = $_POST["player_num"];
-        $gm_password = $_POST["gm_password"];
-        $game_description = "";
+    $gm_email = "";
+    $errored = false;
+    if (isset($_POST["create"])) {
+        $gm_username = htmlspecialchars($_POST['username']);
+        $gm_password = htmlspecialchars($_POST['password']);
+        $player_num = htmlspecialchars($_POST['players']);
+        $game_description = htmlspecialchars($_POST['description']);
 
-        # get information from database here.
-        if (!preg_match('/^[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]+$/', $gm_email)) {
-            $email_error = "Must be valid email";
-
-            //'/^[a-zA-Z0-9]+@[a-zA-Z0-9.]*[a-zA-Z0-9]+.[a-z0-9A-Z]+$/'
-        }
         if (!preg_match('/^[a-zA-Z0-9]+/', $gm_password)) {
-            $passError = "incorrect format for password, must be letters or numbers with no spaces.";
-
+            $error = "Incorrect format for password. Password must be letters or numbers with no spaces.";
+            $errored = true;
         }
+
         if (!preg_match('/^[a-zA-Z0-9]+/', $gm_username)) {
-            $usernameError = "incorrect format for username, must be letters or numbers with no spaces.";
-
-        }
-        if (!preg_match('/^[a-zA-Z0-9. ]*/', $game_description)){
-            $description_error = "Description is suspicious, please watch your use of special characters such as \";\", \"'\", etc.";
+            $error = "Incorrect format for username. Username must be letters or numbers with no spaces.";
+            $errored = true;
         }
 
+        if (!$errored) {
+            require_once "../classes/Game.php";
+            $newGame = NEW Game($gm_username, $gm_password, $player_num, $game_description, true);
+            session_start();
+            $_SESSION['username'] = $newGame->GMusername;
+            $_SESSION['type'] = 'admin';
 
+            header("Location: ../pages/game_page.php");
+        }
     }
-
-
-
-
-
- ?>
-
-    </style>
+    ?>
 </head>
 <body>
-
-<div id="form_container">
-
-    <h1><a>Create Game</a></h1>
-    <form method="post" action="create_game.php" style="margin-bottom: 100px">
-
-        <div class="content">
-            <h2>Create Game</h2>
-
-
-
-
-
-                <label class="description" for="element_1"> </label>
-                <div class=" wide-3">
-
-                    <div class="padded">
-                        <label class="description" for="gm_username">Enter your Username </label>
-
-                        <div>
-                            <input id="gm_username" name="gm_username" class="element text medium" type="text" maxlength="255" value=""/>
-                            <?php
-
-                            echo $usernameError;
-                            ?>
-                        </div>
-                <div class="padded">
-                    <label class="description" for="gm_password">Enter your Desired Password </label>
-                    <div>
-                        <input id="gm_password" name="gm_password" type="password" class="element text medium"  maxlength="255" value=""/>
-                        <?php
-                        echo $passError;
-                        ?>
-                    </div>
+<div class="content">
+    <form method="post" action="create_game.php">
+        <div class="wide-3">
+            <h1>Create Game</h1>
+            <hr>
+            <div class="section">
+                <label for="username">Username:</label><br>
+                <input type="text" id="username" name="username">
+            </div>
+            <div class="section">
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password">
+            </div>
+            <div class="error padded" <?php if (!$errored) {echo "style=\"display: none;\"";} ?>>
+                <div>
+                    <span> <?php echo $error ?> </span>
                 </div>
-        <div class="padded">
-            <label class="description" for="gm_password">Enter your Email </label>
-
+            </div>
+            <div class="section">
+                <label for="players">Number of Players:</label><br>
+                <select id="players" name="players">
+                    <option value="1" selected="selected">One</option>
+                    <option value="2" >Two</option>
+                    <option value="3" >Three</option>
+                    <option value="4" >Four</option>
+                </select>
+            </div>
+            <div class="section">
+                <label for="description">Game Description:</label><br>
+                <textarea id="description" name="description" maxlength="1024" rows="8"></textarea>
+            </div>
+            <div></div>
             <div>
-                <input id="gm_email" name="gm_email" class="element text medium" type="text" maxlength="255" value=""/>
-                <?php
-
-                echo $email_error;
-                ?>
+                <input class="button" type="submit" name="create" value="Create">
             </div>
-        </div>
-                    <div class="padded">
-                        <label class="description" for="game_description">Enter description of Game </label>
-
-                        <div>
-
-                            <textarea id="game_description" name="game_description" class="element text medium" type="text" maxlength="1024" >
-
-
-                            </textarea>
-                            <?php
-
-                            echo $description_error;
-                            ?>
-                        </div>
-                    </div>
-                        <p>Choose the amount of Players</p>
-                        <select class="" id="player_num" name="player_num">
-
-                            <option value="1" selected="selected">One</option>
-                            <option value="2" >Two</option>
-                            <option value="3" >Three</option>
-                            <option value="4" >Four</option>
-
-                        </select>
-
-            <div class="buttons">
-
-
-                <input  class="button" type="submit" name="submit" value="Submit Game" />
-            </div>
-
-        </div>
         </div>
     </form>
-    <?php
-        if(isset($_POST['submit'])) {
-            if (!$description_error && !$passError && !$email_error && !$usernameError) {
-                require_once "../classes/Game.php";
-                $newGame = NEW Game($gm_username, $gm_password, $player_num, $game_description, true);
-                session_start();
-                $_SESSION['username'] = $newGame->GMusername;
-                $_SESSION['type'] = 'admin';
-
-                header("Location: ../pages/game_page.php");
-            }
-        }
-
-    ?>
-
+</div>
 </body>
 </html>

e05b371b548c58938280e13b6062a7c19926fe14
Sage Pellinen <sagepellinen@gmail.com>
2019-04-09 20:17:32 -0500

    Game Table for game page

--- pages/game_page.php
+++ pages/game_page.php
@@ -1,33 +1,53 @@
-<!--Perhaps the admin and player pages should be merged?-->
-
 <!DOCTYPE html>
-<html lang="en-us">
+<html lang="en">
 <head>
+    <title>Game Page</title>
     <link rel="stylesheet" type="text/css" href="../styles/general.css">
-    <title>View Game</title>
+    <style>
+        .content {
+            width: 1024px;
+        }
+
+        td {
+            padding: 1em 1em 1em 1em;
+        }
+        th, td {
+            background: black;
+        }
+        body{
+            background: black;
+        }
+        table {
+            background: white;
+            width: 100%;
+        }
+
+        th {
+
+            height: 50px;
+        }
+
+    </style>
 </head>
+
+
 <body>
 <?php
+
 session_start();
 if(isset($_SESSION['game'])){
     $tempGame = unserialize($_SESSION['game']);
-
     $username = $tempGame->GMusername;
     $gameDescription = $tempGame->gameDescription;
     $_SESSION['username'] = $username;
     $_SESSION['type'] = "admin";
-
 }
 elseif(isset($_SESSION['username']) && isset($_SESSION['type'])){
     $username = $_SESSION['username'];
-
     require_once '../Database_Access/login.php';
-
     $connection = new mysqli($hn, $un, $pw, $db);
     if ($connection->connect_error) die($connection->connect_error);
-
     $query = "SELECT gameID FROM account_table WHERE username = '$username';";
-
     $result = $connection->query($query);
     $rows = $result->fetch_array(MYSQLI_ASSOC);
     $gameID = $rows['gameID'];
@@ -35,16 +55,74 @@ elseif(isset($_SESSION['username']) && isset($_SESSION['type'])){
     $result = $connection->query($query);
     $rows = $result->fetch_array(MYSQLI_ASSOC);
     $gameDescription = $rows['gameDescription'];
-
 }
 
 
 
+
+$ID = $_GET["id"];
+echo ''. $ID . '';
+echo 'Players, Usernames and levels are listed Below';
+
+
+?>
+
+
+
+<div class="content">
+
+
+<table>
+    <tr>
+        <th>Username</th>
+        <th>Character Name</th>
+        <th>Character Level</th>
+    </tr>
+    <?php
+    for(($i = 0);($i<4);($i++))
+    {
+        echo "<tr>";
+        echo "<td>Username</td>";
+        echo "<td>Character Name</td>";
+        echo "<td>Character Level</td>";
+        echo "</tr>";
+    }
+    ?>
+
+
+
+
+        <?php
+
+
+
+
+
+
+        while($row = $result->fetch_array())
+        {
+            echo '<tr>';
+            $value=$row['users'];
+            echo "<td>".$value."</td>" ;
+            $value=$row['characters'];
+            echo "<td>".$value."</td>" ;
+            $value=$row['levels'];
+            echo "<td>".$value."</td>" ;
+            echo '</tr>';
+        }
+
+
+        ?>
+    </table>
+</div>
+</body>
+
+</html>
+    <?php
+
+
 //Present in a table
 // character name, description
-
-
-// Test commit mark
 require_once("../classes/Character.php");
 $users = [];
 $character1 = "";
@@ -62,3 +140,5 @@ echo $game_id;
 ?>
 </body>
 </html>
+
+

ece6d1c9b8c6ada2fa5b6688bc05f6f598579faa
Jesse Parker <jessep6072@gmail.com>
2019-04-09 20:21:24 -0500

    Add First Iteration Of Game_page.php

--- pages/game_page.php
+++ pages/game_page.php
@@ -17,8 +17,38 @@ if(isset($_SESSION['game'])){
     $_SESSION['username'] = $username;
     $_SESSION['type'] = "admin";
 
+    require_once '../Database_Access/login.php';
+
+    $connection = new mysqli($hn, $un, $pw, $db);
+    if ($connection->connect_error) die($connection->connect_error);
+
+    $query = "SELECT username, charID FROM account_table WHERE type = 'player' AND gameID = '$gameID' AND username <> '$username';";
+    $result = $connection->query($query);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+    echo '<p>'.$gameDescription.'</p>';
+
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $charID = $row['charID'];
+        $query = "SELECT charClass FROM character_table WHERE charID = '$charID';";
+        $charClassResult = $connection->query($query);
+        $charClassResult = $charClassResult->fetch_array(MYSQLI_ASSOC);
+        $tempClass = unserialize($charClassResult['charClass']);
+
+        echo '<tr>';
+        $value = $row['username'];
+        echo "<td>" . $value . "</td>";
+        $value = $tempClass->level;
+        echo "<td>" . $value . "</td>";
+        $value = $tempClass->name;
+        echo "<td>" . $value . "</td>";
+        echo '</tr>';
+
+    }
+
 }
 elseif(isset($_SESSION['username']) && isset($_SESSION['type'])){
+
     $username = $_SESSION['username'];
 
     require_once '../Database_Access/login.php';
@@ -36,6 +66,35 @@ elseif(isset($_SESSION['username']) && isset($_SESSION['type'])){
     $rows = $result->fetch_array(MYSQLI_ASSOC);
     $gameDescription = $rows['gameDescription'];
 
+    $query = "SELECT username, charID FROM account_table WHERE type = 'player' AND gameID = '$gameID' AND username <> '$username';";
+    $result = $connection->query($query);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+    echo '<p>'.$gameDescription.'</p>';
+
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $charID = $row['charID'];
+        $query = "SELECT charClass FROM character_table WHERE charID = '$charID';";
+        $charClassResult = $connection->query($query);
+        $charClassResult = $charClassResult->fetch_array(MYSQLI_ASSOC);
+        $tempClass = unserialize($charClassResult['charClass']);
+
+        echo '<tr>';
+        $value = $row['username'];
+        echo "<td>" . $value . "</td>";
+        $value = $tempClass->level;
+        echo "<td>" . $value . "</td>";
+        $value = $tempClass->name;
+        echo "<td>" . $value . "</td>";
+        echo '</tr>';
+
+    }
+
+
+
+}
+else{
+    header("../pages/login_page.php");
 }
 
 
